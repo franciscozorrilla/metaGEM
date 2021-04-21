ac2ff2766585c6bc79c1872cb138e4a46fca1bdb
Francisco Zorrilla <zorrilla@chalmers.se>
2021-04-21 12:05:45 +0100

    feat: add travis CI yml file, fix: activate metagem env before pip installing tools

--- env_setup.sh
+++ env_setup.sh
@@ -55,7 +55,7 @@ fi
 while true; do
     read -p "Do you wish to download and set up metaGEM conda environment? (y/n)" yn
     case $yn in
-        [Yy]* ) echo "mamba env create --prefix ./envs/metagem -f envs/metaGEM_env.yml && pip install --user memote carveme smetana && source deactivate && echo "|bash; break;;
+        [Yy]* ) echo "mamba env create --prefix ./envs/metagem -f envs/metaGEM_env.yml && source activate envs/metagem && pip install --user memote carveme smetana && source deactivate && echo "|bash; break;;
         [Nn]* ) echo -e "\nSkipping metaGEM env setup, note that you will need this for refinement & reassembly of MAGs.\n"; break;;
         * ) echo "Please answer yes or no.";;
     esac
