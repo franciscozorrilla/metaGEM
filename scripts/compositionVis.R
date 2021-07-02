3149ee3d2c26d9a8a25e85dc13d6b1742c89a4b1
Francisco Zorrilla <zorrilla@chalmers.se>
2021-07-02 11:21:47 +0100

    fix: replace old motus-based abundanceVis script with new GTDBTk-based compositionVis (fix issue #63)

---
+++ scripts/compositionVis.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+taxonomy=read.delim("GTDBtk.stats",header=TRUE) %>% 
+  select(user_genome,classification) %>% 
+  separate(.,classification,into = c("kingdom","phylum","class","order","family","genus","species"),sep = ";")
+
+abundance=read.delim("abundance.stats",header=FALSE)
+colnames(abundance)=c("user_genome","absolute_ab","rel_ab")
+
+taxab = left_join(taxonomy,abundance,by="user_genome")
+taxab$sample = gsub("\\..*$","",taxab$user_genome)
+taxab$species = gsub("s__$","Undefined sp.",taxab$species)
+taxab$species = gsub("s__","",taxab$species)
+
+ggplot(taxab%>% filter(species!="Undefined sp.")) +
+  geom_bar(aes(x=reorder_within(species,-rel_ab,sample),y=rel_ab*100),stat="identity") + 
+  scale_x_reordered() +
+  facet_wrap(~sample,scales = "free") + 
+  ylab("Relative abundance (%)") + 
+  xlab("Species") +
+  coord_flip()
+
+ggsave("compositionVis.pdf",width = 12,height=8)
